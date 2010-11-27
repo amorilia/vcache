eaa75f28de513c521eea4f691ab36cc781d88f5e
Amorilia <amorilia@users.sourceforge.net>
2010-11-27 20:20:56 +0000

    Added initial main documentation page.

--- include/vcache.hpp
+++ include/vcache.hpp
@@ -1,10 +1,14 @@
-/*
-Algorithms to reorder triangle list order and vertex order aiming to
-minimize vertex cache misses.
+/*! \mainpage vcache Documentation
 
-This is effectively an implementation of
-'Linear-Speed Vertex Cache Optimisation' by Tom Forsyth, 28th September 2006
+The vcache library finds triangle list orderings which minimize vertex
+cache misses. The implementation is based on Tom Forsyth's
+<em>Linear-Speed Vertex Cache Optimisation</em> article (28th
+September 2006). See
 http://home.comcast.net/~tom_forsyth/papers/fast_vert_cache_opt.html
+for a detailed description and discussion of the algorithm.
+
+\todo Describe basic usage.
+
 */
 
 // ***** BEGIN LICENSE BLOCK *****
