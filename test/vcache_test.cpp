43d2fb0a9720eea1bd30b663efdea56c0eded783
Amorilia <amorilia@users.sourceforge.net>
2010-11-27 14:48:11 +0000

    Added cmake build files and initial test framework.

---
+++ test/vcache_test.cpp
@@ -0,0 +1,12 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
+
+#include "vcache.hpp"
+
+BOOST_AUTO_TEST_SUITE(vcache_test_suite)
+
+BOOST_AUTO_TEST_CASE(dummy_test) {
+}
+
+BOOST_AUTO_TEST_SUITE_END()
