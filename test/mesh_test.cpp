d156e2529be6ee9e8da16750117be61349f5db3b
Amorilia <amorilia@users.sourceforge.net>
2010-12-17 15:20:55 +0000

    Added mesh tests for face-vertex connections.

--- test/mesh_test.cpp
+++ test/mesh_test.cpp
@@ -68,6 +68,119 @@ BOOST_AUTO_TEST_CASE(mesh_add_face_test)
     BOOST_CHECK_EQUAL(m._vertices.size(), 8);
 }
 
+BOOST_AUTO_TEST_CASE(mvertex_mfaces_test_0)
+{
+    // construct mesh
+    Mesh m;
+    MFacePtr f0 = m.add_face(0, 1, 2);
+    MFacePtr f1 = m.add_face(1, 3, 2);
+    MFacePtr f2 = m.add_face(2, 3, 4);
+    MVertexPtr v0 = m._vertices[0].lock();
+    MVertexPtr v1 = m._vertices[1].lock();
+    MVertexPtr v2 = m._vertices[2].lock();
+    MVertexPtr v3 = m._vertices[3].lock();
+    MVertexPtr v4 = m._vertices[4].lock();
+    std::set<MFacePtr> mfaces;
+
+    // 0->-1
+    //  \ / \
+    //   2-<-3
+    //   2->-3
+    //    \ /
+    //     4
+
+    // correct faces?
+    // vertex 0
+    mfaces.clear();
+    mfaces.insert(f0);
+    BOOST_CHECK_EQUAL(lock_set(v0->mfaces), mfaces);
+    // vertex 1
+    mfaces.clear();
+    mfaces.insert(f0);
+    mfaces.insert(f1);
+    BOOST_CHECK_EQUAL(lock_set(v1->mfaces), mfaces);
+    // vertex 2
+    mfaces.clear();
+    mfaces.insert(f0);
+    mfaces.insert(f1);
+    mfaces.insert(f2);
+    BOOST_CHECK_EQUAL(lock_set(v2->mfaces), mfaces);
+    // vertex 3
+    mfaces.clear();
+    mfaces.insert(f1);
+    mfaces.insert(f2);
+    BOOST_CHECK_EQUAL(lock_set(v3->mfaces), mfaces);
+    // vertex 4
+    mfaces.clear();
+    mfaces.insert(f2);
+    BOOST_CHECK_EQUAL(lock_set(v4->mfaces), mfaces);
+}
+
+BOOST_AUTO_TEST_CASE(mvertex_mfaces_test_1)
+{
+    // construct mesh
+    Mesh m;
+    MFacePtr f0 = m.add_face(0, 1, 2);
+    MFacePtr f1 = m.add_face(1, 3, 2);
+    MFacePtr f2 = m.add_face(2, 3, 4);
+    MFacePtr f3 = m.add_face(2, 3, 5);
+    MVertexPtr v0 = m._vertices[0].lock();
+    MVertexPtr v1 = m._vertices[1].lock();
+    MVertexPtr v2 = m._vertices[2].lock();
+    MVertexPtr v3 = m._vertices[3].lock();
+    MVertexPtr v4 = m._vertices[4].lock();
+    MVertexPtr v5 = m._vertices[5].lock();
+    std::set<MFacePtr> mfaces;
+
+    // correct faces?
+    // vertex 0
+    mfaces.clear();
+    mfaces.insert(f0);
+    BOOST_CHECK_EQUAL(lock_set(v0->mfaces), mfaces);
+    // vertex 1
+    mfaces.clear();
+    mfaces.insert(f0);
+    mfaces.insert(f1);
+    BOOST_CHECK_EQUAL(lock_set(v1->mfaces), mfaces);
+    // vertex 2
+    mfaces.clear();
+    mfaces.insert(f0);
+    mfaces.insert(f1);
+    mfaces.insert(f2);
+    mfaces.insert(f3);
+    BOOST_CHECK_EQUAL(lock_set(v2->mfaces), mfaces);
+    // vertex 3
+    mfaces.clear();
+    mfaces.insert(f1);
+    mfaces.insert(f2);
+    mfaces.insert(f3);
+    BOOST_CHECK_EQUAL(lock_set(v3->mfaces), mfaces);
+    // vertex 4
+    mfaces.clear();
+    mfaces.insert(f2);
+    BOOST_CHECK_EQUAL(lock_set(v4->mfaces), mfaces);
+    // vertex 5
+    mfaces.clear();
+    mfaces.insert(f3);
+    BOOST_CHECK_EQUAL(lock_set(v5->mfaces), mfaces);
+}
+
+BOOST_AUTO_TEST_CASE(mvertex_mfaces_test_2)
+{
+    // single triangle mesh
+    Mesh m;
+    MFacePtr f = m.add_face(0, 1, 2);
+    MVertexPtr v0 = m._vertices[0].lock();
+    MVertexPtr v1 = m._vertices[1].lock();
+    MVertexPtr v2 = m._vertices[2].lock();
+    std::set<MFacePtr> mfaces;
+    mfaces.clear();
+    mfaces.insert(f);
+    BOOST_CHECK_EQUAL(lock_set(v0->mfaces), mfaces);
+    BOOST_CHECK_EQUAL(lock_set(v1->mfaces), mfaces);
+    BOOST_CHECK_EQUAL(lock_set(v2->mfaces), mfaces);
+}
+
 /*
 BOOST_AUTO_TEST_CASE(triangle_info_test)
 {
--- test/test_helpers.hpp
+++ test/test_helpers.hpp
@@ -5,27 +5,78 @@
 #include <list>
 #include <set>
 
+#include "vcache/mesh.hpp"
+
 namespace std
 {
 
-ostream & operator<<(ostream & stream, set<int> const & data)
+template <typename T>
+ostream & operator<<(ostream & stream, set<T> const & data)
 {
     stream << "std::set( ";
-    BOOST_FOREACH(int elem, data) {
+    BOOST_FOREACH(T const & elem, data) {
         stream << elem << " ";
     };
     stream << ")";
     return stream;
 };
 
-ostream & operator<<(ostream & stream, list<int> const & data)
+template <typename T>
+ostream & operator<<(ostream & stream, boost::shared_ptr<T> const & data)
+{
+    stream << "boost::shared_ptr( & ";
+    if (data) {
+        stream << *data << " ";
+    } else {
+        stream << "NULL ";
+    }
+    stream << ")";
+    return stream;
+};
+
+template <typename T>
+ostream & operator<<(ostream & stream, boost::weak_ptr<T> const & data)
+{
+    stream << "boost::weak_ptr( & ";
+    boost::shared_ptr<T> result = data.lock();
+    if (result) {
+        stream << *result << " ";
+    } else {
+        stream << "NULL ";
+    }
+    stream << ")";
+    return stream;
+};
+
+ostream & operator<<(ostream & stream, MFace const & mface)
+{
+    stream << "MFace( ";
+    stream << mface.mv0->vertex << " " << mface.mv1->vertex << " " << mface.mv2->vertex;
+    stream << ")";
+    return stream;
+};
+
+template <typename T>
+ostream & operator<<(ostream & stream, list<T> const & data)
 {
     stream << "std::list( ";
-    BOOST_FOREACH(int elem, data) {
+    BOOST_FOREACH(T const & elem, data) {
         stream << elem << " ";
     };
     stream << ")";
     return stream;
 };
+
+// lock all elements of a set of weak_ptr's (this is needed before
+// checking equality)
+template <typename T>
+set<boost::shared_ptr<T> > lock_set(set<boost::weak_ptr<T> > const & data)
+{
+    set<boost::shared_ptr<T> > result;
+    BOOST_FOREACH(boost::weak_ptr<T> const & elem, data) {
+        result.insert(elem.lock());
+    }
+    return result;
+}
 
 }
