0b62a292097844186a9e27acda935ff0c7d2e173
Amorilia <amorilia@users.sourceforge.net>
2011-11-20 10:55:18 +0000

    License text updated for 2011.

--- include/vcache/vertex_score.hpp
+++ include/vcache/vertex_score.hpp
@@ -1,6 +1,6 @@
 /*
 
-Copyright (c) 2007-2010, Python File Format Interface
+Copyright (c) 2007-2011, Python File Format Interface
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
